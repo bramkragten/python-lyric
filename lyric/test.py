4ba70d72d8afd63883297af2d9bc5c61e1dc2ded
Bram Kragten <brakra@Heemskerk.local>
2017-01-23 13:03:28 +0100

    removed client id and secret

--- lyric/test.py
+++ lyric/test.py
@@ -1,14 +1,14 @@
 import lyric
 
-client_id = r'j9ShGaDTsOWlykvAeJCNcMpO76gGwGq6'
-client_secret = r'vtt3qfspAhzgLLyU'
+client_id = r''
+client_secret = r''
 
 redirect_uri = 'https://hass.deproducties.com:8123/lyric'
 app_name = 'Sample'
 token_cache_file = 'token.txt'
 
-lapi = lyric.Lyric(client_id=client_id, client_secret=client_secret, 
-                   token_cache_file=token_cache_file, 
+lapi = lyric.Lyric(client_id=client_id, client_secret=client_secret,
+                   token_cache_file=token_cache_file,
                    redirect_uri=redirect_uri, app_name=app_name)
 
 if lapi._token is None:
